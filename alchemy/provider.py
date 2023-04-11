0d25e6ee76f8b71f574ce5fdf7d5d8e75d6f86af
Eric Ranasinghe <ericranasinghe@gmail.com>
2023-04-11 18:40:18 +0300

    * added virtual id to physical id logic * added reconnect and resubscribe * added some docstrings

--- alchemy/provider.py
+++ alchemy/provider.py
@@ -1,6 +1,6 @@
+import itertools
 import json
 import threading
-import time
 import uuid
 from typing import Any, Union, Optional
 
@@ -61,211 +61,209 @@ class AlchemyProvider(JSONBaseProvider):
 
 
 class AlchemyWebsocketProvider:
+    """
+    A WebSocket provider for Alchemy that supports subscribing to events.
+    """
+
     def __init__(self, config: AlchemyConfig):
+        self.request_counter = itertools.count()
         self.ws_thread = None
         self.url = config.get_request_url(AlchemyApiType.WSS)
         self.ws = websocket.WebSocket()
-        self._listeners = {"logs": [], "newHeads": [], "newPendingTransactions": []}
-        self.virtual_to_subscription = {}
-        # self.start_block = {
-        #     "logs": None,
-        #     "newHeads": None,
-        #     "newPendingTransactions": None,
-        # }
-        self.connect()
+        self._listeners = {'logs': [], 'newHeads': [], 'newPendingTransactions': []}
+        self.virtual_subscriptions_by_id = {}
+        # self.connect()
 
     @backoff.on_exception(
-        backoff.expo, websocket.WebSocketConnectionClosedException, max_tries=10
+        backoff.expo,
+        websocket.WebSocketConnectionClosedException,
+        max_tries=5,
+        max_value=30,
+        on_backoff=lambda details: print(
+            f"Connection failed, retrying in {details['wait']} seconds..."
+        ),
+        on_giveup=lambda details: print('Max retries reached, giving up.'),
     )
     def connect(self):
+        """
+        Connect to the WebSocket and start a new thread to listen for messages.
+        """
+        if self.ws.connected:
+            return
+
         self.ws.connect(self.url)
         self.ws_thread = threading.Thread(target=self._listen_for_messages)
         self.ws_thread.start()
-        self._resubscribe_all()
+        if self.virtual_subscriptions_by_id:
+            self._resubscribe()
 
     def _listen_for_messages(self):
-        while self.is_connected():
-            message = self.ws.recv()
-            if message:
-                self._on_message(self.ws, message)
-            else:
-                self.ws.close()
-                break
-
-    def _resubscribe_all(self):
-        for event_type, listeners in self._listeners.items():
-            for listener in listeners:
-                virtual_id = listener["virtual_id"]
-                if virtual_id not in self.virtual_to_subscription:
-                    self._subscribe(event_type, listener["callback"], virtual_id)
-
-    # def _backfill(self):
-    #     self.ws.send(json.dumps({"id": 1, "method": "eth_blockNumber", "params": []}))
-    #     result = json.loads(self.ws.recv())
-    #     latest_block_number = int(result['result'], 16)
-    #
-    #     self.ws.send(
-    #         json.dumps(
-    #             {
-    #                 "id": 2,
-    #                 "method": "eth_getBlockByNumber",
-    #                 "params": [hex(latest_block_number), True],
-    #             }
-    #         )
-    #     )
-    #     result = json.loads(self.ws.recv())
-    #     latest_block = result['result']
-    #
-    #     if "newHeads" in self.listeners:
-    #         for listener in self.listeners["newHeads"]:
-    #             listener({"event": "newHeads", "result": latest_block})
-    #
-    #     if "logs" in self.listeners:
-    #         for log in latest_block["transactions"]:
-    #             for listener in self.listeners["logs"]:
-    #                 listener({"event": "logs", "result": log})
-    #
-    #     self.ws.send(
-    #         json.dumps({"id": 3, "method": "eth_pendingTransactions", "params": []})
-    #     )
-    #     result = json.loads(self.ws.recv())
-    #     pending_transactions = result['result']
-    #
-    #     if "newPendingTransactions" in self.listeners:
-    #         for tx in pending_transactions:
-    #             for listener in self.listeners["newPendingTransactions"]:
-    #                 listener({"event": "newPendingTransactions", "result": tx})
-
-    def _on_open(self, ws):
-        pass
-
-    def _on_message(self, ws, message):
-        data = json.loads(message)
-        if "params" in data and "subscription" in data["params"]:
-            subscription_id = data["params"]["subscription"]
-            for event_type, listeners in self._listeners.items():
-                for listener in listeners:
-                    if listener["subscription_id"] == subscription_id:
-                        listener["callback"](data["params"]["result"])
+        """
+        Listen for messages from the WebSocket and handle them.
+        """
+        try:
+            while self.is_connected():
+                try:
+                    message = self.ws.recv()
+                    if message:
+                        self._handle_message(message)
+                    else:
+                        self.ws.close()
                         break
-
-    def _on_error(self, ws, error):
-        print(f"WebSocket error: {error}")
-        self.ws.close()
-
-    def _on_close(self, ws, close_status_code, close_msg):
-        print(f"Connection closed: {close_status_code}, {close_msg}")
-        # self.reconnect()
-
-    def reconnect(self, max_retries=4):
-        if self.is_connected():
+                except websocket.WebSocketException as e:
+                    print(f'WebSocket error: {e}')
+                    self.ws.close()
+                    break
+        except Exception as e:
+            print(f'Error in _listen_for_messages: {e}')
             self.ws.close()
+        finally:
+            self.connect()
 
-        delay = 1
-        retries = 0
-        while retries < max_retries:
-            try:
-                self.connect()
-                break
-            except Exception as e:
-                print(f"Reconnection failed: {e}")
-                time.sleep(delay)
-                delay = min(delay * 2, 30)
-                retries += 1
+    def _handle_message(self, message):
+        """
+        Handle incoming WebSocket messages by executing the appropriate listener.
+        """
+        data = json.loads(message)
+        if 'params' in data and 'subscription' in data['params']:
+            subscription_id = data['params']['subscription']
+            for event_type, listeners in self._listeners.items():
+                listener_executed = False
+                for listener in listeners:
+                    if listener['subscription_id'] == subscription_id:
+                        listener['callback'](data['params']['result'])
+                        listener_executed = True
+                        break
+                if listener_executed:
+                    break
+
+    def _subscribe(self, event_type, *args):
+        """
+        Subscribe to the specified event type.
+        """
+        self.ws.send(
+            json.dumps(
+                {
+                    'jsonrpc': '2.0',
+                    'id': next(self.request_counter),
+                    'method': 'eth_subscribe',
+                    'params': [event_type],
+                }
+            )
+        )
+        response = self.ws.recv()
+        subscription_id = json.loads(response).get('params').get('subscription')
+        return subscription_id
+
+    def _unsubscribe(self, subscription_id):
+        """
+        Unsubscribe from the specified subscription ID.
+        """
+        self.ws.send(
+            json.dumps(
+                {
+                    'jsonrpc': '2.0',
+                    'id': next(self.request_counter),
+                    'method': 'eth_unsubscribe',
+                    'params': [subscription_id],
+                }
+            )
+        )
+        response = self.ws.recv()
+        return json.loads(response).get('result')
+
+    def _resubscribe(self):
+        """
+        Resubscribe to all virtual subscriptions.
+        """
+        for (
+            virtual_id,
+            virtual_subscription,
+        ) in self.virtual_subscriptions_by_id.items():
+            event_type = virtual_subscription['event_type']
+            callback = virtual_subscription['callback']
+
+            physical_id = self._subscribe(event_type, callback)
+            virtual_subscription['subscription_id'] = physical_id
 
     def on(self, event_type, callback):
+        """
+        Add an event listener for the specified event type.
+        """
+        self.connect()
         if event_type not in self._listeners:
-            raise ValueError("Invalid event type")
+            raise ValueError('Invalid event type')
 
-        return self._subscribe(event_type, callback)
+        subscription_id = self._subscribe(event_type, callback)
+        virtual_id = str(uuid.uuid4())
+        listener = {
+            'event_type': event_type,
+            'subscription_id': subscription_id,
+            'virtual_id': virtual_id,
+            'callback': callback,
+            'params': [event_type],
+        }
+        self._listeners[event_type].append(listener)
+        self.virtual_subscriptions_by_id[virtual_id] = listener
+        return virtual_id
 
     def once(self, event_type, callback):
+        """
+        Add a one-time event listener for the specified event type.
+        """
+        self.connect()
         if event_type not in self._listeners:
-            raise ValueError("Invalid event type")
+            raise ValueError('Invalid event type')
 
         def wrapped_callback(result):
             self.off(event_type, wrapped_callback)
             callback(result)
 
-        self._subscribe(event_type, wrapped_callback)
+        return self.on(event_type, wrapped_callback)
 
     def off(self, event_type, callback=None):
+        """
+        Remove an event listener for the specified event type.
+        """
         if event_type not in self._listeners:
-            raise ValueError("Invalid event type")
+            raise ValueError('Invalid event type')
 
         if callback is not None:
             for listener in self._listeners[event_type]:
-                if listener["callback"] == callback:
-                    self._unsubscribe(listener["subscription_id"])
-                    self._listeners[event_type].remove(listener)
+                if listener['callback'] == callback:
+                    result = self._unsubscribe(listener['subscription_id'])
+                    if result:
+                        self._listeners[event_type].remove(listener)
+                        del self.virtual_subscriptions_by_id[listener['virtual_id']]
                     break
         else:
-            for listener in list(self._listeners[event_type]):
-                self._unsubscribe(listener["subscription_id"])
-                self._listeners[event_type].remove(listener)
-
-    def _subscribe(self, event_type, callback, virtual_id=None):
-        if event_type not in self._listeners:
-            raise ValueError("Invalid event type")
-
-        # if virtual_id not in self.virtual_to_subscription:
-        request_id = str(uuid.uuid4())
-        self.ws.send(
-            json.dumps(
-                {
-                    "jsonrpc": "2.0",
-                    "id": request_id,
-                    "method": "eth_subscribe",
-                    "params": [event_type],
-                }
-            )
-        )
-        response = self.ws.recv()
-        subscription_id = json.loads(response).get("params").get('subscription')
-
-        listener = {
-            "event_type": event_type,
-            "subscription_id": subscription_id,
-            "callback": callback,
-        }
-        self._listeners[event_type].append(listener)
-
-        # self.virtual_to_subscription[virtual_id] = {
-        #     "event_type": event_type,
-        #     "subscription_id": subscription_id,
-        # }
-        # return virtual_id
-        return subscription_id
-
-    def _unsubscribe(self, subscription_id):
-        request_id = str(uuid.uuid4())
-        self.ws.send(
-            json.dumps(
-                {
-                    "jsonrpc": "2.0",
-                    "id": request_id,
-                    "method": "eth_unsubscribe",
-                    "params": [subscription_id],
-                }
-            )
-        )
-        response = self.ws.recv()
-        return json.loads(response).get("result")
+            self.remove_all_listeners(event_type)
 
     def remove_all_listeners(self, event_type=None):
+        """
+        Remove all event listeners for the specified event type or
+        for all event types if none is specified.
+        """
         if event_type is None:
             for event, listeners in self._listeners.items():
                 for listener in listeners:
-                    self._unsubscribe(listener["subscription_id"])
-            self._listeners = {"logs": [], "newHeads": [], "newPendingTransactions": []}
+                    self._unsubscribe(listener['subscription_id'])
+            self._listeners = {'logs': [], 'newHeads': [], 'newPendingTransactions': []}
+            self.virtual_subscriptions_by_id = {}
+            self.ws.close()
         elif event_type in self._listeners:
             for listener in self._listeners[event_type]:
-                self._unsubscribe(listener["subscription_id"])
+                self._unsubscribe(listener['subscription_id'])
+                del self.virtual_subscriptions_by_id[listener['virtual_id']]
             self._listeners[event_type] = []
         else:
-            raise ValueError("Invalid event type")
+            raise ValueError('Invalid event type')
 
     def listener_count(self, event_type=None):
+        """
+        Get the number of listeners for the specified event type or
+        for all event types if none is specified.
+        """
         if event_type is None:
             total_listeners = sum(
                 [len(listeners) for listeners in self._listeners.values()]
@@ -274,19 +272,26 @@ class AlchemyWebsocketProvider:
         elif event_type in self._listeners:
             return len(self._listeners[event_type])
         else:
-            raise ValueError("Invalid event type")
+            raise ValueError('Invalid event type')
 
     def listeners(self, event_type=None):
+        """
+        Get all event listeners for the specified event type or
+        for all event types if none is specified.
+        """
         if event_type is None:
             all_listeners = {
-                key: [listener["callback"] for listener in value]
+                key: [listener['callback'] for listener in value]
                 for key, value in self._listeners.items()
             }
             return all_listeners
         elif event_type in self._listeners:
-            return [listener["callback"] for listener in self._listeners[event_type]]
+            return [listener['callback'] for listener in self._listeners[event_type]]
         else:
-            raise ValueError("Invalid event type")
+            raise ValueError('Invalid event type')
 
     def is_connected(self):
+        """
+        Check if the WebSocket is connected.
+        """
         return self.ws is not None and self.ws.connected
